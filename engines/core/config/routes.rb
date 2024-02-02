20962ff3101db5f5ed70b933c2dbddbd795dab37
Jacques Marques <jmarques1@gmail.com>
2024-02-02 16:38:34 -0300

    feat: create admin controller and route

--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,5 +1,9 @@
 Blast::Core::Engine.routes.draw do
   devise_for :users, class_name: 'Blast::User', module: :devise
 
+  namespace :admin do
+    get '/' => 'admin#index'
+  end
+
   root to: 'dashboard#index'
 end
